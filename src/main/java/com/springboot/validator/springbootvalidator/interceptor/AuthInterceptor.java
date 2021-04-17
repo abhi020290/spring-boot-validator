dfb9316a51fda9f92bc249d368597d0cd89d5540
abhi020290 <abhi020290@gmail.com>
2021-04-17 13:32:51 +0530

    Added roles allowed

--- src/main/java/com/springboot/validator/springbootvalidator/interceptor/AuthInterceptor.java
+++ src/main/java/com/springboot/validator/springbootvalidator/interceptor/AuthInterceptor.java
@@ -1,23 +1,66 @@
 package com.springboot.validator.springbootvalidator.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.ContainerServlet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+import java.util.Map;
 
 @Component
 @Slf4j
 public class AuthInterceptor implements HandlerInterceptor {
 
+    @Value("${xRequest.id.value}")
+    String xRequestId;
+
+
+    @Value("${xClient.id.value}")
+    String xClientId;
+
+    @Value("#{${rollowedAllowedForClient}}")
+    private Map<String, String> valuesMap;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("AuthInterceptor Interceptor preHandle method Invoked ************");
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            if(headerName.equalsIgnoreCase("X-REQUEST-ID")){
+                if(!headerValue.equals(xRequestId)){
+                    log.info("AuthInterceptor failed X-REQUEST-ID");
+                    return false;
+                }
+            }
+            if(headerName.equalsIgnoreCase("X-CLIENT-ID")){
+                if(!headerValue.equals(xClientId)){
+                    log.info("AuthInterceptor failed X-CLIENT-ID");
+                    return false;
+                }
+            }
+
+        }
+        checkRoles(request);
+        log.info("AuthInterceptor success X-CLIENT-ID & X-REQUEST-ID");
         return true;
+    }
+
+    private void checkRoles(HttpServletRequest request) {
+        Class<? extends HttpServletRequest> aClass = request.getClass();
+        RolesAllowed annotation = aClass.getAnnotation(RolesAllowed.class);
+        String[] value = annotation.value();
+        valuesMap.toString();
     }
 
     @Override
--- src/main/java/com/springboot/validator/springbootvalidator/resource/EmployeeResource.java
+++ src/main/java/com/springboot/validator/springbootvalidator/resource/EmployeeResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 
 @RestController
@@ -16,11 +17,13 @@ public class EmployeeResource {
     private PropertyBean propertyBean;
 
     @PostMapping("employee")
+    @RolesAllowed(value = "employee")
     ResponseEntity<Employee> handleInput(@Valid @RequestBody Employee employee){
         return ResponseEntity.ok(employee);
     }
 
     @GetMapping("message")
+    @RolesAllowed(value = "message")
     ResponseEntity<String> handleMessage(){
         return ResponseEntity.ok(propertyBean.getMessage());
     }
